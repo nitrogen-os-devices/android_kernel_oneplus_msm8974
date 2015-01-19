83863f42f55bfd128cc41be8e5b87f922670e7dd
intersectRaven <rjgolo@gmail.com>
2015-01-19 14:55:20 +0800

    Disable deferment during full tickless mode.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2991,7 +2991,7 @@ void scheduler_tick(void)
 }
 
 #ifdef CONFIG_NO_HZ_FULL
-static u32 sched_tick_max_deferment = HZ;
+static u32 sched_tick_max_deferment = -1;
 
 /**
  * scheduler_tick_max_deferment
