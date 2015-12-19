342dfcc684b36c3f46651fa237b38280c2f27e28
nikhil18 <nikhilgolechha1@gmail.com>
2015-12-19 19:51:14 +0530

    Revert "posix timers: Allocate timer id per process (v2)"
    This reverts commit 2ca73f07a595d20bfa899d795068c03ebc0d6655.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -580,8 +580,7 @@ struct signal_struct {
 	unsigned int		has_child_subreaper:1;
 
 	/* POSIX.1b Interval Timers */
-	int			posix_timer_id;
-	struct list_head	posix_timers;
+	struct list_head posix_timers;
 
 	/* ITIMER_REAL timer for the process */
 	struct hrtimer real_timer;
