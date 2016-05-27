839a956dc4415992a57f69f15a082cba2fa74005
eray orçunus <erayorcunus@gmail.com>
2016-05-27 19:34:29 +0300

    msm: ion: Allow non contiguous allocations in secure heap.
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    
    Conflicts:
    	drivers/staging/android/uapi/linux/msm_ion.h

--- drivers/staging/android/uapi/linux/msm_ion.h
+++ drivers/staging/android/uapi/linux/msm_ion.h
@@ -66,6 +66,14 @@ enum cp_mem_usage {
 	UNKNOWN = 0x7FFFFFFF,
 };
 
+#define ION_HEAP_TYPE_DMA_MASK         (1 << ION_HEAP_TYPE_DMA)
+
+ /**
+ * Flag to allow non continguous allocation of memory from secure
+ * heap
+ */
+#define ION_FLAG_ALLOW_NON_CONTIG (1 << 24)
+
 /**
  * Flag to use when allocating to indicate that a heap is secure.
  */
